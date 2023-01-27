7c576a88ec34979f7f2579462a7aaa164f33c0c8
parkjunyeongg <amfydrkfl2@gmail.com>
2023-01-27 09:44:05 +0900

    ReactCloak v1.0

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Mydiv1 from './Mydiv1';
+import Mydiv2 from './Mydiv2';
+import MyClock from './MyClock';
+
+const App =() => {
+ 
+  return (
+    <div className="App-header">
+      <Mydiv1 />
+      <Mydiv2 />
+      <MyClock />
+    </div>
+   
+  );
+
+
+}
+
+export default App;
